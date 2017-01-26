aac7e2ec7f08bb78e2c747f98c624b2167ef41c9
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2017-01-26 15:18:06 +0100

    update readme

---
+++ app/starter-vue-js/src/components/Started.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="started">
+  </div>
+</template>
+
+<script>
+// import { } from '../vuex/actions';
+// import { } from '../vuex/getters';
+
+export default {
+  name: 'started',
+  components: {},
+  data () {
+    return {}
+  },
+  vuex: {
+    actions: {
+    },
+    getters: {
+    }
+  },
+  watch: {},
+  created: function() {},
+	mounted: function() {},
+  methods:{}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixins.scss";
+@import "../styles/variables.scss";
+
+  .started{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+    background-color: gray;
+  }
+
+</style>
