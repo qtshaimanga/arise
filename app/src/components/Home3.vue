6220d2c2776e08c4e92443cc1b68eb5cebb26371
DeWin Bebebtos <DeWin Bebebtos>
2017-02-03 13:20:51 +0100

    site demo

---
+++ app/src/components/Home3.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="home3">
+    <div class="container">
+      <p>VIDEO</p>
+      <video :src="getListOfRessources.teaser.file.src" width="900px"  height="auto "ref="videoBox" class="video-txt" @click="initVideo();"></video>
+    </div>
+  </div>
+</template>
+
+<script>
+// import {  } from '../vuex/actions';
+import { getListOfRessourcesState } from '../vuex/getters';
+
+export default {
+  name: 'home3',
+  components: {
+  },
+  data () {
+    return {}
+  },
+  vuex: {
+    actions: {
+    },
+    getters:{
+      getListOfRessources: getListOfRessourcesState
+    }
+  },
+  watch: {
+
+  },
+  created: function(){
+  },
+	mounted: function() {
+
+  },
+  methods:{
+    initVideo: function(){
+      this.$refs.videoBox.play();
+      this.$refs.videoBox.loop = false;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixins.scss";
+@import "../styles/variables.scss";
+
+  .home3{
+    width: 100%;
+    height: 90%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  .container{
+    margin-top: 100px;
+    width: 100%;
+    height: 90%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    p{
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: auto;
+      color: $grey-pale;
+      font-family: "mongoose-regular";
+      font-size: 35pt;
+    }
+    video{
+      width: 800px;
+      height: auto;
+      margin:auto;
+      &:hover{
+        cursor: pointer;
+      }
+    }
+  }
+
+</style>
---
+++ app/src/components/Home8.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home8">
+    <transition name="fade" appear>
+      <div class="container">
+        <img :src="getListOfRessources.gobelins.file.src"/>
+        <p>Arise is a school project made at Gobelins, l'école de</br> l'image, during a three weeks IOT workshop</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+// import {  } from '../vuex/actions';
+import { getListOfRessourcesState } from '../vuex/getters';
+
+export default {
+  name: 'home8',
+  components: {
+  },
+  data () {
+    return {}
+  },
+  vuex: {
+    actions: {
+    },
+    getters:{
+      getListOfRessources: getListOfRessourcesState
+    }
+  },
+  watch: {
+
+  },
+  created: function(){
+  },
+	mounted: function() {
+  },
+  methods:{
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/mixins.scss";
+@import "../styles/variables.scss";
+
+  .home8{
+    width: 100%;
+    height: 70%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  .container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    img{
+      margin: auto;
+      margin-top: 180px;
+    }
+    p{
+      text-align: center;
+      margin: auto;
+      margin-top: 20px;
+      font-family: "cinetype-regular";
+      font-size: 12pt;
+      color: $grey-pale-opacity;
+    }
+  }
+
+</style>
