204fb1edba42dbfbafb0d62efcea4d7399b26046
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2017-02-03 11:12:30 +0100

    add video lock in ressources

--- app/src/services/resources.js
+++ app/src/services/resources.js
@@ -40,6 +40,11 @@ export default [
     url: '/dist/assets/videos/invitation.mp4'
   },
   {
+    type: 'mp4',
+    id: 'lock_landing_video',
+    url: '/dist/assets/videos/lock.mp4'
+  },
+  {
     type: 'text',
     id: 'cross_blue',
     url: '/dist/assets/icons/cross-blue.svg'
